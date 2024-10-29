56c013cf93c00567c8105f5e86f54f775fd84d08
Tyuiu.BuynovaVI@outlook.com <v.buyanova1@mail.ru>
2024-10-30 01:25:31 +0500

    Task5

---
+++ Tyuiu.BuyanovaVI.Sprint3.Task5.V24.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿
+using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.BuyanovaVI.Sprint3.Task5.V24.Lib
+{
+    public class DataService : ISprint3Task5V24
+    {
+        public double GetSumSumSeries(int x, int startValue1, int startValue2, int stopValue1, int stopValue2)
+        {
+            double sumSeries = 0;
+            for (int i = startValue1; i <= stopValue1; i++)
+            {
+                for (int k = startValue2; k <= stopValue2; k++)
+                {
+                    sumSeries = sumSeries + (((Math.Pow(x, k)) + 2) * (Math.Sin(k)));
+                }
+            }
+            return (Math.Round(sumSeries, 3));
+        }
+    }
+}
